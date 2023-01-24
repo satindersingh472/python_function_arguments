73c0d5837fef1bd5f65dc3a37352b04be7f9b33a
satindersingh772 <92008199+satindersingh772@users.noreply.github.com>
2023-01-24 10:25:41 -0700

    done_commenting

--- app.py
+++ app.py
@@ -4,14 +4,17 @@ tables = {
     3: []
 }
 
+# this function will accept positional arguments 
 def assign_tables_positional(table_number, name, vip_status):
     tables[table_number] = [name,vip_status]
     return tables
 
+# this function is for keyword arguments
 def assign_tables_keywords(table_number, name, vip_status):
     tables[table_number] = [name,vip_status]
     return tables
 
+# this function will use some parameter default if not passed while calling function
 def assign_tables_default(table_number, name, vip_status = False):
     tables[table_number] = [name, vip_status]
     return tables
